d14f8a1674d219ce2e905e11386c553769c0f204
MartkCz <antik494@gmail.com>
2018-07-05 10:21:57 +0200

    Use kdyby/strict-objects instead of deprecated nette/utils class

--- src/StrictObject.php
+++ src/StrictObject.php
@@ -1,35 +1,81 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace WebChemistry;
 
-use Nette\MemberAccessException;
-use Nette\Utils\ObjectHelpers;
+use Kdyby\StrictObjects\Scream;
 
 trait StrictObject {
 
-	public function __get(string $name) {
-		ObjectHelpers::strictGet(get_class($this), $name);
+	use Scream {
+		Scream::__get as private screamGet;
+		Scream::__set as private screamSet;
+		Scream::__call as private screamCall;
+		Scream::__isset as private screamIsset;
+		Scream::__callStatic as private screamCallStatic;
+		Scream::__unset as private screamUnset;
 	}
 
-	public function __set(string $name, $value) {
-		ObjectHelpers::strictSet(get_class($this), $name);
+	/**
+	 * Call to undefined method.
+	 *
+	 * @param string $name method name
+	 * @param array $args arguments
+	 * @return mixed
+	 */
+	public function __call(string $name, array $args) {
+		$this->screamCall($name, $args);
 	}
 
-	public function __call(string $name, array $arguments) {
-		ObjectHelpers::strictCall(get_class($this), $name);
+	/**
+	 * Call to undefined static method.
+	 *
+	 * @param string $name method name (in lower case!)
+	 * @param array $args arguments
+	 * @return mixed
+	 */
+	public static function __callStatic(string $name, array $args) {
+		self::screamCallStatic($name, $args);
 	}
 
-	public function __unset(string $name) {
-		$class = get_class($this);
-		if (!ObjectHelpers::hasProperty($class, $name)) {
-			throw new MemberAccessException("Cannot unset the property $class::\$$name.");
-		}
+	/**
+	 * Returns property value. Do not call directly.
+	 *
+	 * @param string $name property name
+	 * @return mixed
+	 */
+	public function &__get(string $name) {
+		$this->screamGet($name);
 	}
 
-	public static function __callStatic(string $name, array $arguments) {
-		ObjectHelpers::strictCall(get_called_class(), $name);
+	/**
+	 * Sets value of a property. Do not call directly.
+	 *
+	 * @param string $name property name
+	 * @param mixed $value property value
+	 */
+	public function __set(string $name, $value): void {
+		$this->screamSet($name, $value);
+	}
+
+	/**
+	 * Is property defined?
+	 *
+	 * @param string $name property name
+	 * @return bool
+	 */
+	public function __isset(string $name): bool {
+		$this->screamIsset($name);
+
+		return false;
+	}
+
+	/**
+	 * Access to undeclared property.
+	 *
+	 * @param string $name property name
+	 */
+	public function __unset(string $name): void {
+		$this->screamUnset($name);
 	}
 
 }
